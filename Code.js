a1fa13795622e0b9b88dd4730df78561f6cd2154
Deandra Putri <deandraputri637@gmail.com>
2026-03-21 19:51:52 +0700

    month update

--- Code.js
+++ Code.js
@@ -1,6 +1,6 @@
 // CENTRALIZED CONFIGURATION
 const CONFIG = {
-  SHEET_NAME: 'Feb26',
+  SHEET_NAME: 'Mar26',
   ACCOUNTS: ["BCA", "OCTO Pay", "OCTO Savers", "SHOPEE", "GOPAY", "OCTO Bonds", "OVO", "SAQU"],
   CATEGORIES: ["Personal", "Utang ayah & ibu", "Investasi", "Coffee Business", "Pindah duit", "Transportation"],
   DEFAULT_ACCOUNT: "BCA",
